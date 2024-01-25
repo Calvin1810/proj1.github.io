e24860034b2bda34af3078eda96697cb47be344b
Calvin Lu <calvinlu1810@gmail.com>
2024-01-24 23:20:22 -0500

    1.0

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import * as Plot from "@observablehq/plot";
+import * as d3 from "d3";
 
 interface Player {
   name: string;
@@ -11,38 +12,92 @@ interface Player {
   playoff_mpg: number;
 }
 
+interface Case {
+  state: string;
+  year: string;
+  bot_type: string;
+  toxin_type: string;
+  count: string;
+  region: string;
+}
+
+interface Pop {
+  name: string;
+  geography: string;
+  year: string;
+  population: string;
+  percent_change: string;
+  resident_pop: string;
+  reps: string;
+  rep_change: string;
+  apportionment: string
+}
+
 async function main(): Promise<void> {
-  const res = await fetch("data/players_2023.json");
-  const data = (await res.json()) as Array<Player>;
+  // const res = await fetch("data/players_2023.json");
+  // const data = (await res.json()) as Array<Player>;
+  const data: Array<Case> = await d3.csv("data/Botulism_20240122.csv");
+  const pop_data: Array<Pop> = await d3.csv("data/population.csv");
+
   const barchart = Plot.plot({
-    title: "gh pages branch take two",
-    subtitle: "Field goals made & attempted in 2022-23",
-    width: 640,
+    inset: 8,
     grid: true,
+    color: {
+      legend: true,
+      label: "Number of Cases",
+      // type: "categorical",
+      scheme: "OrRd"
+    },
     x: {
-      label: "field goals attempted",
+      ticks: d3.range(1910, 2020, 10),
+      tickFormat: d => d.toString().replace(",", "")
     },
     y: {
-      label: "field goals made",
+      label: "Number of Cases"
     },
     marks: [
-      Plot.dot(data, {
-        x: "fga",
-        y: "fgm",
-        fill: "team_abbreviation",
-      }),
-      Plot.tip(
-        data,
-        Plot.pointer({
-          x: "fga",
-          y: "fgm",
-          title: (d) => `${d.player_name}\n${d.team_abbreviation}`,
-        })
-      ),
-      Plot.ruleY([0]),
-    ],
-  });
+      // Plot.barY(data, {x: "Year", y: "Count", fill: "Count"})
+      Plot.barY(data, Plot.groupX({y: "count", fill: "count"}, {x: "Year", tip: true})),
+      //Plot.line(pop_data_US, {x: "Year", y: "Population"}),
+      Plot.ruleY([0])
+    ]
+  })
+
   document.querySelector("#plot")?.append(barchart);
+
+  // const new_data = data.filter(d => d.year === "2010");
+
+  // //console.log(data);
+  // console.log(data[0].year);
+
+  // const adjusted_data = new_data.map(d => {
+  //   const popObj = pop_data.find(
+  //     pd => pd.name === d.region && pd.year === d.year
+  //   );
+  //   return {...d, Count: Number(d.count) / (popObj ? Number(popObj.population) : 1)};
+  // });
+
+  // const region_chart = Plot.plot({
+  //   inset: 8,
+  //   grid: true,
+  //   color: {
+  //     legend: true,
+  //     type: "categorical",
+  //     scheme: "OrRd"
+  //   },
+  //   y: {
+  //     label: "Total Cases Per Capita"
+  //   },
+  //   marks: [
+  //     Plot.barY(new_data,
+  //               Plot.groupX({y: "count"},
+  //                           {x: "Region", fill: "BotType", sort: {x: "-y"}, tip: true})),
+  //     Plot.ruleY([0])
+  //   ]
+  // });
+
+  // document.querySelector("#plot")?.append(region_chart);
+
 }
 
 window.addEventListener("DOMContentLoaded", async (_evt) => {
